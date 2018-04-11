34d90ac1f4eaff51c466064663db7e97823e7f84
Limengyang3G4G <2679218013@qq.com>
2018-04-11 15:48:57 +0800

    1,李梦阳提交作业

---
+++ S2阶段项目/爱考项目/12李梦阳/AKExamOnline/src/com/qhit/lh/g4/lmy/exam/common/dao/UserDao.java
@@ -0,0 +1,6 @@
+package com.qhit.lh.g4.lmy.exam.common.dao;
+
+import com.qhit.lh.g4.lmy.exam.common.bean.User;
+public interface UserDao {
+	public User login(User user);
+}
---
+++ S2阶段项目/爱考项目/12李梦阳/AKExamOnline/src/com/qhit/lh/g4/lmy/exam/common/utils/Constans.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+package com.qhit.lh.g4.lmy.exam.common.utils;
+public class Constans {
+	public static final String VIEW_LOGIN = "login_view";
+
+}
